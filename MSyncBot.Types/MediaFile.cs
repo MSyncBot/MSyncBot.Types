13a2e1452f43188f2a9785fd880cbe4db29e33cc
Neelov Eugene <neelov.ev@mail.ru>
2023-12-20 14:37:34 +0300

    Deleted unused enum

--- MSyncBot.Types/MediaFile.cs
+++ MSyncBot.Types/MediaFile.cs
@@ -2,10 +2,9 @@
 
 namespace MSyncBot.Types;
 
-public class MediaFile(string name, string extension, byte[] data, FileType fileType)
+public class MediaFile(string name, string extension, byte[] data)
 {
     public string Name { get; set; } = name;
     public string Extension { get; set; } = extension;
     public byte[] Data { get; set; } = data;
-    public FileType FileType { get; set; } = fileType;
 }
