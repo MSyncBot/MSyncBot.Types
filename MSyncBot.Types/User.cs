60dec2a8f8b13ac4c70ed5ec6390c7fab641225c
Neelov Eugene <neelov.ev@mail.ru>
2023-12-23 23:34:10 +0300

    Update User.cs. Make Id and AvatarUrl optional parameters

--- MSyncBot.Types/User.cs
+++ MSyncBot.Types/User.cs
@@ -2,14 +2,14 @@
 
 public class User(
     string firstName,
-    ulong id,
-    string avatarUrl,
     string? lastName = null,
-    string? username = null)
+    string? username = null,
+    ulong? id = null,
+    string? avatarUrl = null)
 {
     public string FirstName { get; set; } = firstName;
-    public ulong Id { get; set; } = id;
-    public string AvatarUrl { get; set; } = avatarUrl;
     public string? LastName { get; set; } = lastName;
     public string? Username { get; set; } = username;
+    public ulong? Id { get; set; } = id;
+    public string? AvatarUrl { get; set; } = avatarUrl;
 }
