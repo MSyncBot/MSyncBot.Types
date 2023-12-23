2e0a705c2e250fbcd59e5d4694c972630ae2064b
Neelov Eugene <neelov.ev@mail.ru>
2023-12-23 14:21:24 +0300

    Added AvatarUrl in User.cs

--- MSyncBot.Types/User.cs
+++ MSyncBot.Types/User.cs
@@ -1,9 +1,15 @@
 ﻿namespace MSyncBot.Types;
 
-public class User(string firstName, string? lastName = null, string? username = null, ulong? id = null)
+public class User(
+    string firstName,
+    ulong id,
+    string avatarUrl,
+    string? lastName = null,
+    string? username = null)
 {
     public string FirstName { get; set; } = firstName;
+    public ulong Id { get; set; } = id;
+    public string AvatarUrl { get; set; } = avatarUrl;
     public string? LastName { get; set; } = lastName;
     public string? Username { get; set; } = username;
-    public ulong? Id { get; set; } = id;
 }
